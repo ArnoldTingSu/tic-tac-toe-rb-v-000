7d3290ff5d5c4be835f6d55cc4589f55d12ff08f
Arnold Su <arnoldtingsu@gmail.com>
2018-09-20 07:56:09 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -14,6 +14,42 @@ WIN_COMBINATIONS = [
   [6, 4, 2]
 ]
 
+def input_to_index(input)
+    input.to_i - 1
+  end
+
+def valid_move?(board,index)
+index.between?(0,8) && !position_taken?(board,index)
+
+end
+
+def position_taken?(board, index)
+
+ board[index] != " "
+
+ end
+
+def move(board, index, token ="X")
+
+  board[index] = token
+
+end
+
+  def turn(board)
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(board,index)
+      move(board,index)
+      display_board(board)
+    else turn(board)
+  end
+    
+    
+    
+  end
+
+
 def won?(board)
   WIN_COMBINATIONS.each {|win_combo|
     index_0 = win_combo[0]
